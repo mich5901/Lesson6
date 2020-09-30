40f7bc1a2c31ca5220ed0a32dbf02e93b95034af
mich5901 <mich5901@ISS2V4PKH2.scdsb.pri>
2020-09-30 12:34:53 -0400

    Lesson 6

--- src/guessgame/GuessMachine.java
+++ src/guessgame/GuessMachine.java
@@ -3,31 +3,22 @@ package guessgame;
 public class GuessMachine {
 
     private int number;
-    private int numGuess;
-    private String giveResult;
+    private int setGuess;
+    private int myGuess;
 
     public GuessMachine() {
-        numGuess = 0;
-        number = (int) (Math.random() % 100) + 1;
+        setGuess = 0;
+        number = (int) (Math.random() * 100) + 1;
+        myGuess = 0;
     }
 
-    public boolean guessNumber(int num) {
-        numGuess++;
-        if (num == number) {
-            return true;
-        } else {
+    public boolean setGuess(int num) {
+        if (num > 100 || num < 0) {
             return false;
         }
-    }
+        myGuess = num;
+        setGuess++;
 
-    public String giveResult(int num) {
-        if (number == number) {
-            return "You got it!";
-        } else if (num < number) {
-            return "Too low. Try again!";
-        }
-        else return "Too high.";
+        return true;
     }
-    
-
 }
